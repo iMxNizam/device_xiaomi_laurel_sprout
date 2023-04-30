57ce691951d61b41076531ee82e4c096d42a60fa
PowerX-NOT <pugalg100@gmail.com>
2023-05-01 01:51:49 +0530

    laurel_sprout: Kang fingerprint from cepheus

--- device.mk
+++ device.mk
@@ -199,7 +199,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.laurel_sprout
+    android.hardware.biometrics.fingerprint@2.3-service.laurel_sprout \
+    android.hardware.biometrics.fingerprint@2.3.vendor \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # FM
 PRODUCT_PACKAGES += \
