a3a039f0ec7c6c921b0dd23ab6253cc46253a04a
PowerX-NOT <pugalg100@gmail.com>
2023-06-12 18:05:04 +0530

    laurel_sprout: Fix display animation while AOD on/off screen

--- properties.mk
+++ properties.mk
@@ -187,8 +187,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.enable_fb_ubwc=1 \
     debug.sf.disable_client_composition_cache=1 \
     debug.hwui.use_hint_manager=true \
-    debug.hwui.target_cpu_time_percent=30
-
+    debug.hwui.target_cpu_time_percent=30 \
+    debug.force_no_blanking=true
+ 
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.dpmhalservice.enable=1
