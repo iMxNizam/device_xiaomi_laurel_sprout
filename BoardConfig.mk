PowerX-NOT/EvoX

laurel_sprout: Add Moto Dolby
Values taken from OP9 Pro and modified by Sasha (helenius147 on Telegram)
to work with Moto Dolby.

- Additionally remove dax-default.xml from proprietary-files.txt
- Improve audio_effects.xml changes
- Move props from product.prop to vendor.prop
- Dolby: We have 2 Output channels for Speakers
- Disable AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP
- ro.control_privapp_permissions=log -> force
- Remove media_codecs_dolby_audio.xml, because we are not having backend for this.
- Remove libswvqe Deps, they are not existing here for MotoDolby
- Adapt for Enforcing A12.1 Sepolicy
- Add # Dolby declarations for Sepolicy

Co-authored-by: blankaf <realblankaf@gmail.com>
Co-authored-by: MB <mb150996@gmail.com>
Co-authored-by: johnmart19 <johnivan19999@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Change-Id: I02e5c2c8b40ab5128c0f26e059406e892319d4c7

BoardConfig.mk
@@ -185,6 +185,7 @@ VENDOR_SECURITY_PATCH := 2022-08-01
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble

iMxNizam/device_xiaomi_laurel_sprout

BoardConfig.mk
@@ -179,6 +179,7 @@ VENDOR_SECURITY_PATCH := 2022-08-01
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble